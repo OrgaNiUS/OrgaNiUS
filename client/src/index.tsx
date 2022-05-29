17bb3c0dfe45a884f780c80a7f202afdb3d647af
Saraan <saraankannusami@gmail.com>
2022-05-29 20:25:00 +0800

    added PageDoesNotExist and UnauthorisedAccess

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,13 +8,15 @@ import { AuthProvider } from "./context/AuthProvider";
 
 export { default as Navbar } from "./components/Navbar";
 export { default as User } from "./pages/User";
-export { default as Page404 } from "./pages/Page404";
 export { default as Homepage } from "./pages/Homepage";
 export { default as Projects } from "./pages/Projects";
 export { default as Settings } from "./pages/Settings";
 export { default as Login } from "./pages/Login";
 export { default as Registration } from "./pages/Registration";
 export { default as ForgotPwd } from "./pages/ForgotPwd";
+export { default as Page404 } from "./pages/ErrorPages/Page404";
+export {default as UnauthorisedAccess} from "./pages/ErrorPages/UnauthorisedAccess";
+export {default as PageDoesNotExist} from "./pages/ErrorPages/PageDoesNotExist";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
