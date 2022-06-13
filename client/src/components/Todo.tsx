097771ef7339319c5a309365fc7438afb3b867ef
Jin Wei <seetohjinwei@gmail.com>
2022-06-13 19:43:33 +0800

    expand todo button

--- client/src/components/Todo.tsx
+++ client/src/components/Todo.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import StylesMerger from "../styles/StyleMerging";
 import styles from "../styles/Todo.module.css";
 import { ITask } from "../types";
+import Modal from "./Modal";
 import Task from "./Task";
 
 const styler = StylesMerger(styles);
@@ -48,21 +50,55 @@ const tasks: ITask[] = [
     // },
 ];
 
+const TodoModal: JSX.Element = <div>I am the Todo expanded view.</div>;
+
 const Todo = (): JSX.Element => {
+    const [showModal, setShowModal] = useState<boolean>(false);
+
     // TODO: for now, there is no filtering mechanism, so this is just a placeholder.
+    // TODO: filtering mechanism
     const filteredTasks: ITask[] = tasks;
 
+    // TODO: Todo Modal View
+
+    const handleExpand = () => {
+        setShowModal(true);
+    };
+
     return (
-        <div className={styler("container")}>
-            <h1 className={styler("title")}>To-Do</h1>
-            {filteredTasks.length === 0 ? (
-                <div>Nothing here!</div>
-            ) : (
-                filteredTasks.map((task, i) => {
-                    return <Task key={i} {...{ task }} />;
-                })
-            )}
-        </div>
+        <>
+            <Modal {...{ active: showModal, body: TodoModal, callback: () => setShowModal(false) }} />
+            <div className={styler("wrapper")}>
+                <div className={styler("container")}>
+                    <h1 className={styler("title")}>To-Do</h1>
+                    {filteredTasks.length === 0 ? (
+                        <div>Nothing here!</div>
+                    ) : (
+                        filteredTasks.map((task, i) => {
+                            return <Task key={i} {...{ task }} />;
+                        })
+                    )}
+                </div>
+                <button className={styler("expand-button")}>
+                    {/* "arrows-expand" from https://heroicons.com/ */}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        onClick={handleExpand}
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4"
+                        />
+                    </svg>
+                </button>
+            </div>
+        </>
     );
 };
 
