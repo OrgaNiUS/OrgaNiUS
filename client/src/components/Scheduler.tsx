a6a5f0d33d30b73899bee370400d635543c74f44
Jin Wei <seetohjinwei@gmail.com>
2022-06-14 15:03:29 +0800

    installed moment + react-big-calendar

--- client/src/components/Scheduler.tsx
+++ client/src/components/Scheduler.tsx
@@ -1,10 +1,21 @@
-import StylesMerger from "../styles/StyleMerging";
+import moment from "moment";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import styles from "../styles/Scheduler.module.css";
+import StylesMerger from "../styles/StyleMerging";
 
+// react-big-calendar's css file
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+// moment is required for react-big-calendar
+const localizer = momentLocalizer(moment);
 const styler = StylesMerger(styles);
 
 const Scheduler = (): JSX.Element => {
-    return <div className={styler("container")}></div>;
+    return (
+        <div className={styler("container")}>
+            <Calendar localizer={localizer} events={[]} startAccessor="start" endAccessor="end" />
+        </div>
+    );
 };
 
 export default Scheduler;
