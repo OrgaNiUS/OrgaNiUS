5d8f7c4ad3a807a2ac42513a1b02e0ae3d92e64e
Saraan <saraankannusami@gmail.com>
2022-07-07 17:09:52 +0800

    Changes to models

--- server/models/project.go
+++ server/models/project.go
@@ -7,14 +7,15 @@ import (
 )
 
 type Project struct {
-	Id           primitive.ObjectID  `bson:"_id,omitempty" json:"id,omitempty"`
-	Name         string              `bson:"name" json:"name"`
-	Description  string              `bson:"description" json:"description"`
-	Members      map[string]struct{} `bson:"members" json:"members"`
-	Tasks        map[string]struct{} `bson:"tasks" json:"tasks"`
-	State        string              `bson:"state" json:"state"`
-	CreationTime time.Time           `bson:"creationTime" json:"creationTime"`
-	Settings     ProjectSettings     `bson:"settings" json:"settings"`
+	Id           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name         string             `bson:"name" json:"name"`
+	Description  string             `bson:"description" json:"description"`
+	Members      map[string]string  `bson:"members" json:"members"` // Key: UserID, Value: Role
+	Tasks        []string           `bson:"tasks" json:"tasks"`
+	CreationTime time.Time          `bson:"creationTime" json:"creationTime"`
+	Settings     ProjectSettings    `bson:"settings" json:"settings"`
+	Applications []string           `bson:"applications" json:"applications"` // [userid]
+	IsPublic     bool               `bson:"isPublic" json:"isPublic"`
 }
 
 type ProjectSettings struct {
@@ -23,6 +24,7 @@ type ProjectSettings struct {
 }
 
 type Permissions struct {
+	IsAdmin         bool `bson:"isAdmin" json:"isAdmin"`
 	AddMember       bool `bson:"addMember" json:"addMember"`
 	RemoveMember    bool `bson:"removeMember" json:"removeMember"`
 	EditName        bool `bson:"editName" json:"editName"`
@@ -35,10 +37,16 @@ type Permissions struct {
 
 func DefaultSettings() ProjectSettings {
 	settings := ProjectSettings{
-		// This is the zero time. can check if time is initialised with time.isZero() method
-		DeadlineNotification: time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC),
+		// This is the zero time. can check if time is initialised with time.isZero() method.
+		// DeadlineNotification: time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC), COMMMENTED OUT, ZERO TIME SHOULD BE AUTOASSIGNED if undefined
 	}
 	settings.Roles = make(map[string]Permissions)
-	settings.Roles["admin"] = Permissions{true, true, true, true, true, true, true, true}
+	settings.Roles["admin"] = Permissions{
+		IsAdmin: true,
+	}
+	settings.Roles["member"] = Permissions{
+		AddTask:    true,
+		RemoveTask: true,
+	}
 	return settings
 }
