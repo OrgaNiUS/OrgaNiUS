e24119084a80f15dea2a81c0878b49d79f3617eb
Saraan <saraankannusami@gmail.com>
2022-07-19 04:39:40 +0800

    Project Settings and Leave project

--- client/src/pages/Project.tsx
+++ client/src/pages/Project.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
-import { ProjectDelete, ProjectModify } from "../api/ProjectAPI";
+import { ProjectDelete, ProjectLeave, ProjectModify, ProjectRemoveUser } from "../api/ProjectAPI";
 import Modal from "../components/Modal";
 import PreLoader from "../components/PreLoader";
 import ProjectsInvite from "../components/Projects/ProjectsInvite";
@@ -11,8 +11,9 @@ import { TodoProvider } from "../components/Todo/TodoProvider";
 import AuthContext from "../context/AuthProvider";
 import { DataContext } from "../context/DataProvider";
 import { filterTaskOptions, mergeEventArrays } from "../functions/events";
-import { BaseButton } from "../styles";
-import { IEvent, IProject, ITask } from "../types";
+import { BaseButton, NoEffectButton } from "../styles";
+import { IEvent, IProject, ITask, IUser } from "../types";
+import { removeFromArray } from "../functions/arrays";
 
 const Title = styled.h1`
     font-size: 2rem;
@@ -61,8 +62,13 @@ const Button2 = styled(Button)`
 
 const ButtonDeleteProject = styled(BaseButton)`
     background-color: rgb(255, 0, 90);
-    width: 80%;
+    width: 45%;
 `;
+
+const ButtonLeaveProject = styled(BaseButton)`
+    background-color: rgb(255, 0, 90);
+`;
+
 const ButtonConfirmDelete = styled(BaseButton)`
     background-color: rgb(255, 0, 90);
     border: 1px solid rgb(255, 0, 90);
@@ -94,8 +100,13 @@ const Project = (): JSX.Element => {
     const [newName, setNewName] = useState<string>();
     const [newDesc, setNewDesc] = useState<string>();
     const [isPublic, setPublic] = useState<boolean>();
+    const [currUsers, setCurrUsers] = useState<Array<IUser>>([]);
+    const [removedUsers, setRemovedUsers] = useState<Array<IUser>>([]);
+
     const [showSettingsModal, setShowSettingsModal] = useState<boolean>(false);
     const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+    const [showRemModal, setShowRemModal] = useState<boolean>(false);
+    const [showLeaveModal, setShowLeaveModal] = useState<boolean>(false);
 
     const doneTrigger = (task: ITask) => {
         data.patchTask(
@@ -189,26 +200,34 @@ const Project = (): JSX.Element => {
             setProject(project);
             setTasks(tasks);
             setLoading(false);
-            setPublic(project?.isPublic)
         });
 
         // Debugging purposes
         // setProject({
-        //     id: "",
+        //     id: "5215321",
         //     name: "Trial Project",
-        //     description: "sample description",
-        //     members: [],
+        //     description: "testsfasfasafsa description",
+        //     members: [
+        //         { id: "123111", name: "USER1", role: "admin" },
+        //         { id: "123222", name: "USER2", role: "member" },
+        //         { id: "123333", name: "USER3", role: "member" },
+        //     ],
         //     events: [],
         //     tasks: [],
         //     creationTime: new Date(),
         //     isPublic: false,
         // });
         // setLoading(false);
-        // setPublic(true);
-
         // including data.getProject and id will cause this to continuously fire
         // eslint-disable-next-line
     }, []);
+
+    useEffect(() => {
+        if (project) {
+            setCurrUsers(project.members.filter((user) => user.id !== auth.auth.id));
+            setPublic(project.isPublic);
+        }
+    }, [project]);
 
     if (project === undefined || projectid === undefined) {
         return (
@@ -261,17 +280,48 @@ const Project = (): JSX.Element => {
         );
     };
 
-    const handleDelete: React.MouseEventHandler<HTMLButtonElement> = (event) => { 
+    const handleDelete: React.MouseEventHandler<HTMLButtonElement> = (event) => {
         event.preventDefault();
         ProjectDelete(
             auth.axiosInstance,
             { projectid: projectid },
-            () => { 
-                navigate("/projects")
+            () => {
+                navigate("/projects");
+                window.location.reload();
             },
             () => {}
-        )
-    }
+        );
+    };
+
+    const handleRemoveUsers: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+        ProjectRemoveUser(
+            auth.axiosInstance,
+            { projectid: projectid, userids: removedUsers.map((user) => user.id) },
+            () => {
+                setShowRemModal(false);
+                setShowSettingsModal(true);
+                window.location.reload();
+            },
+            () => {
+                setShowRemModal(false);
+                setShowSettingsModal(true);
+            }
+        );
+    };
+
+    const handleLeaveProject: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.preventDefault();
+        ProjectLeave(
+            auth.axiosInstance,
+            { projectid: projectid },
+            () => {
+                navigate("/projects");
+                window.location.reload();
+            },
+            () => {}
+        );
+    };
 
     const SettingsModal = (
         <>
@@ -303,6 +353,19 @@ const Project = (): JSX.Element => {
                     </Button>
                 </div>
                 <div className="mt-2">
+                    <Button2
+                        onClick={() => {
+                            setShowRemModal(true);
+                            setShowSettingsModal(false);
+                        }}
+                    >
+                        Edit Users
+                    </Button2>
+                    <Button2>
+                        <Link to={`/project_applications/${projectid}`}>User Join Requests</Link>
+                    </Button2>
+                </div>
+                <div className="mt-2">
                     {isPublic ? (
                         <Button2 type="submit" onClick={handleChangePublic}>
                             Make Private
@@ -312,16 +375,89 @@ const Project = (): JSX.Element => {
                             Make Public
                         </Button2>
                     )}
-                    <Button2 type="submit">Remove Users</Button2>
+                    <ButtonDeleteProject
+                        onClick={() => {
+                            setShowDeleteModal(true);
+                            setShowSettingsModal(false);
+                        }}
+                    >
+                        Delete Project
+                    </ButtonDeleteProject>
                 </div>
-                <ButtonDeleteProject
-                    onClick={() => {
-                        setShowDeleteModal(true);
-                        setShowSettingsModal(false);
-                    }}
-                >
-                    Delete Project
-                </ButtonDeleteProject>
+            </div>
+        </>
+    );
+
+    const RemoveUsersModal = (
+        <>
+            <h1 className="mb-1 text-2xl">Edit Users</h1>
+            <div className="w-full flex-col" key={currUsers.length}>
+                <div className="w-full ">
+                    Current Users:
+                    <div className="w-80">
+                        {currUsers.map((User) => {
+                            return (
+                                <>
+                                    <li className="flex flex-row mt-1 border-2 items-center rounded" key={User.id}>
+                                        <div className="flex justify-items-start flex-grow">{User.name}</div>
+                                        <Button
+                                            className="flex justify-end"
+                                            onClick={() => {
+                                                let tempRemovedUsers = removedUsers;
+                                                tempRemovedUsers.push(User);
+                                                setRemovedUsers(tempRemovedUsers);
+                                                let tempCurrUsers = currUsers;
+                                                tempCurrUsers = removeFromArray(tempCurrUsers, User);
+                                                setCurrUsers(tempCurrUsers);
+                                            }}
+                                        >
+                                            Remove
+                                        </Button>
+                                    </li>
+                                </>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="w-full flex-col">
+                    Users to be Removed:
+                    <div className="w-80">
+                        {removedUsers.map((User) => {
+                            return (
+                                <>
+                                    <li className="flex flex-row mt-1 border-2 items-center rounded" key={User.id}>
+                                        <div className="flex justify-items-start flex-grow">{User.name}</div>
+                                        <Button
+                                            onClick={() => {
+                                                let tempRemovedUsers = removedUsers;
+                                                let tempCurrUsers = currUsers;
+                                                tempCurrUsers.push(User);
+                                                setCurrUsers(tempCurrUsers);
+                                                tempRemovedUsers = removeFromArray(tempRemovedUsers, User);
+                                                setRemovedUsers(tempRemovedUsers);
+                                            }}
+                                        >
+                                            Don't Remove
+                                        </Button>
+                                    </li>
+                                </>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <Button2
+                        onClick={() => {
+                            setShowRemModal(false);
+                            setShowSettingsModal(true);
+                            setCurrUsers(project.members);
+                            setRemovedUsers([]);
+                        }}
+                    >
+                        Cancel
+                    </Button2>
+                    <Button2 onClick={handleRemoveUsers}>Confirm</Button2>
+                </div>
             </div>
         </>
     );
@@ -352,6 +488,38 @@ const Project = (): JSX.Element => {
                 onClick={() => {
                     setShowDeleteModal(false);
                     setShowSettingsModal(true);
+                }}
+            >
+                Cancel
+            </ButtonConfirmCancel>
+        </>
+    );
+
+    const LeaveModal = (
+        <>
+            <div>
+                <svg
+                    // SVG from https://heroicons.com/
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-8 pr-2 align-bottom inline"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                    />
+                </svg>
+                Are you sure you want to leave this project?
+            </div>
+            <div>You will need to apply/get invited to join the project again.</div>
+            <ButtonConfirmDelete onClick={handleLeaveProject}>Confirm</ButtonConfirmDelete>
+            <ButtonConfirmCancel
+                onClick={() => {
+                    setShowLeaveModal(false);
                 }}
             >
                 Cancel
@@ -392,32 +560,49 @@ const Project = (): JSX.Element => {
                         },
                     }}
                 />
+                <Modal
+                    {...{
+                        active: showRemModal,
+                        body: RemoveUsersModal,
+                        callback: () => {
+                            setShowRemModal(false);
+                            setCurrUsers(project.members);
+                            setRemovedUsers([]);
+                        },
+                    }}
+                />
+                <Modal
+                    {...{
+                        active: showLeaveModal,
+                        body: LeaveModal,
+                        callback: () => {
+                            setShowLeaveModal(false);
+                        },
+                    }}
+                />
                 <Container>
                     {showInviteWindow && <ProjectsInvite {...{ projectid, setShowInviteWindow }} />}
                     <Row className="my-2">
                         <Link to="/projects">⬅️ Back to Projects</Link>
-                        <ButtonArray>
+                        <ButtonArray className="border-2 rounded border-orange-400 bg-orange-300">
                             {isAdmin ? (
                                 <Button onClick={() => setShowSettingsModal(true)}>Settings</Button>
                             ) : (
-                                <Button disabled>Settings</Button>
+                                <NoEffectButton disabled className="bg-slate-400">
+                                    Settings
+                                </NoEffectButton>
                             )}
-                            <Button disabled={!isAdmin}>
-                                {isAdmin ? (
-                                    // show link only if is admin (else just regular disabled button)
-                                    <Link to={`/project_applications/${projectid}`}>Applications</Link>
-                                ) : (
-                                    "Applications"
-                                )}
-                            </Button>
                             <Button onClick={() => setShowInviteWindow(true)} disabled={!isAdmin}>
                                 Invite
                             </Button>
+                            <ButtonLeaveProject onClick={() => {
+                                setShowLeaveModal(true);
+                            }}>Leave Project</ButtonLeaveProject>
                         </ButtonArray>
                     </Row>
                     <Row>
                         <LeftBox>
-                            <Title>{project.name}</Title>
+                            <Title className="underline underline-offset-auto">{project.name}</Title>
                             <ProjectContent>{project.description}</ProjectContent>
                             <ProjectContent>Members: {project.members.map((m) => m.name).join(", ")}</ProjectContent>
                         </LeftBox>
