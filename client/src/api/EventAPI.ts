d3b610c18a19493fe619ec559d283c984a98e3ec
Jin Wei <seetohjinwei@gmail.com>
2022-07-20 11:26:01 +0800

    client event apis

---
+++ client/src/api/EventAPI.ts
@@ -0,0 +1,45 @@
+import {
+    CreateDeleteFunctionWithParams,
+    CreateGetFunctionWithParams,
+    CreatePatchFunction,
+    CreatePostFunction,
+} from "./API";
+
+type EventCreateParams = {
+    name: string;
+    start: string; // ISO 8601 format
+    end: string; // ISO 8601 format
+    projectid?: string;
+};
+export const EventCreate = CreatePostFunction<EventCreateParams>("/event_create");
+
+type EventGetParams = {
+    eventid: string;
+};
+export const EventGet = CreateGetFunctionWithParams<EventGetParams>("/event_get");
+
+// Get All User Events: Leave projectid as empty string
+// Get All Project Events: Put relevant projectid
+type EventGetAllParams = {
+    projectid: string;
+};
+export const EventGetAll = CreateGetFunctionWithParams<EventGetAllParams>("/event_get_all");
+
+type EventPatchParams = {
+    eventid: string;
+    name?: string;
+    start?: string; // ISO 8601 format
+    end?: string; // ISO 8601 format
+};
+export const EventPatch = CreatePatchFunction<EventPatchParams>("/event_modify");
+
+type EventDeleteParams = {
+    eventid: string;
+    projectid?: string; // if associated with a project
+};
+export const EventDelete = CreateDeleteFunctionWithParams<EventDeleteParams>("/event_delete");
+
+type EventNusmodsParams = {
+    url: string;
+};
+export const EventNusmods = CreatePostFunction<EventNusmodsParams>("/event_nusmods");
